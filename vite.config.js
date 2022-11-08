4e0cb8038e150b1d4f0d89afb9300d0cee63ac15
omarjesusperezortiz <omarjesus.perezortiz@gmail.com>
2022-11-08 20:32:17 +0100

    fix2

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: { outDir: 'docs' },
-  publicPath: process.env.NODE_ENV === 'production' ? '/task-app/' : '/',
-  base: process.env.NODE_ENV === 'production' ? '/task-app/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   server: {
-    base: process.env.NODE_ENV === 'production' ? '/task-app/' : '/',
+    base: process.env.NODE_ENV === 'production' ? '/' : '/',
 }
 })
